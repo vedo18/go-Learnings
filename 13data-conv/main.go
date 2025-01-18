ea984e3d2dd08ae12ad88fc5f33f76af612e8d09
vedo18 <saketvats18@gmail.com>
2025-01-18 16:51:36 +0530

    data conv -> int and string

--- 13data-conv/main.go
+++ 13data-conv/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
+	//int to float64
 	var num int = 40
 	fmt.Println("Value is", num)
 	fmt.Printf("Type is %T\n", num)
@@ -11,4 +15,23 @@ func main() {
 	data = data + 1.31
 	fmt.Println("Value is", data)
 	fmt.Printf("Type is %T\n", data)
+
+	//int to string
+	num1 := 40
+	fmt.Println("Value is", num1)
+	fmt.Printf("Type is %T\n", num1)
+
+	data1 := strconv.Itoa(num1)
+	fmt.Println("Value is", data1)
+	fmt.Printf("Type is %T\n", data1)
+
+	//string to int
+	data2 := "123456789"
+	fmt.Println("Value is", data2)
+	fmt.Printf("Type is %T\n", data2)
+
+	num2, _ := strconv.Atoi(data2)
+	fmt.Println("Value is", num2)
+	fmt.Printf("Type is %T\n", num2)
+
 }
