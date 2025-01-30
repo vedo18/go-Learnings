13f2397868dec06fc9739b697e97e31a56447518
vedo18 <saketvats18@gmail.com>
2025-01-30 15:37:30 +0530

    defer in golang

--- 16defer/main.go
+++ 16defer/main.go
@@ -4,8 +4,19 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello golang")
+
+	// Schedule "Defer example1" to be printed just before the main function returns.
+	// The defer statement ensures this line will execute after all other code in the main function.
 	defer fmt.Println("Defer example1")
+
+	// Schedule "Defer example 2" to be printed just before the main function returns.
+	// This defer statement is added to the stack of deferred calls.
 	defer fmt.Println("Defer example 2")
 
 	fmt.Println("Program end here")
+
+	// At this point, the main function is about to return.
+	// The deferred functions are executed in Last-In-First-Out (LIFO) order:
+	// 1. "Defer example 2" (last deferred) is executed first.
+	// 2. "Defer example1" (first deferred) is executed next.
 }
